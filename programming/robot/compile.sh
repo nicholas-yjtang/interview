2983d547add59baa2067f59178aa0433cda605b3
Nicholas Tang <nicholas.tang@knocaminolta.com>
2023-06-29 04:51:23 +0800

    added the uml diagrams

--- programming/robot/compile.sh
+++ programming/robot/compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir -p build
-cp -r src/* build
+cp -r src/*.cpp build
+cp -r src/*.h build
+cp -r src/CMakeLists.txt build
 pushd build
 cmake .
 make -j
+./robot
 popd
-mvn build
+mvn compile
 mvn test
